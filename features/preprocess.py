abe134df4484cbeecfcb6c5bb46d893370c1f444
varun <vembarvarun@gmail.com>
2018-04-24 16:56:11 +0530

    add ntlk word tokenizer

--- features/preprocess.py
+++ features/preprocess.py
@@ -1,15 +1,24 @@
 import pandas as pd
 import contractions
+import nltk
+
 
 
 class ArticleLoader():
     def __init__(self, file_path='../data/tmp/articles/wololo.csv'):
-        self.articles = pd.read_csv(file_path)['article_text'].replace('\n', ' ', regex=True).tolist()
+        self.articles = pd.read_csv(file_path)['article_text'].sample(5).replace('\n', ' ', regex=True).tolist()
 
 
 class Preprocess():
     def __init__(self, article_list):
         try:
             self.contractions_fixed = [contractions.fix(z) for z in [y for y in article_list]]
+            self.tokens_list = self.article_tokenizer(self.contractions_fixed)
         except Exception as e:
-            print(article_list)
+            print(e)
+    def article_tokenizer(self,article_list,method='nltk'):
+        if method == 'nltk':
+            return [ nltk.word_tokenize(x) for x in article_list ]
+
+articles = ArticleLoader().articles
+y = Preprocess(articles).article_tokenizer(articles)
