5353c1b9ca399d66eb4a04496fb9e59bb81536cb
varun <vembarvarun@gmail.com>
2018-04-24 17:22:27 +0530

    Remove stop words - from NLTK corpus

--- features/preprocess.py
+++ features/preprocess.py
@@ -17,8 +17,15 @@ class Preprocess():
         except Exception as e:
             print(e)
     def article_tokenizer(self,article_list,method='nltk'):
+        stop_words = nltk.corpus.stopwords.words('english')
+        stopped_tokens_removed = []
         if method == 'nltk':
-            return [ nltk.word_tokenize(x) for x in article_list ]
+            for curr_text in article_list:
+                tokens = nltk.word_tokenize(curr_text)
+                y = [token for token in tokens if not token in stop_words]
+                stopped_tokens_removed.append(y)
+        return stopped_tokens_removed
 
 articles = ArticleLoader().articles
 y = Preprocess(articles).article_tokenizer(articles)
+print(articles[2],'\n',y[2])
