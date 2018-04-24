9579153d94a4085214bba62270ef7970ea70a18c
varun <vembarvarun@gmail.com>
2018-04-24 15:38:49 +0530

    Fix contractions #TODO:Use better library

--- features/preprocess.py
+++ features/preprocess.py
@@ -1,9 +1,15 @@
 import pandas as pd
-import nltk
+import contractions
+
 
 class ArticleLoader():
-    def __init__(self, file_path='../data/tmp/site_responses/complete_html.csv'):
-        self.articles= pd.read_csv(file_path)['article_text'].sample(1).replace('\n',' ',regex=True).tolist()
+    def __init__(self, file_path='../data/tmp/articles/wololo.csv'):
+        self.articles = pd.read_csv(file_path)['article_text'].replace('\n', ' ', regex=True).tolist()
 
 
-x = ArticleLoader().articles[0]
+class Preprocess():
+    def __init__(self, article_list):
+        try:
+            self.contractions_fixed = [contractions.fix(z) for z in [y for y in article_list]]
+        except Exception as e:
+            print(article_list)
