61b6699b0865e1e7854ab24d1a26612f0c4179d4
varun <vembarvarun@gmail.com>
2018-04-26 13:59:45 +0530

    Comment out fixing contractions in init, Add function to tokenize and selectively remove or retain stopwords.

--- features/preprocess.py
+++ features/preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import contractions
 import nltk
-
+from nltk.corpus import stopwords
 
 class ArticleLoader():
     def __init__(self, file_path='../data/tmp/articles/wololo.csv'):
@@ -9,20 +9,29 @@ class ArticleLoader():
 
 
 class Preprocess():
+    '''
     def __init__(self, article_list):
         try:
             self.contractions_fixed = [contractions.fix(z) for z in [y for y in article_list]]
             self.tokens_list = self.article_tokenizer(self.contractions_fixed)
         except Exception as e:
             print(e)
+    '''
 
-    def article_tokenizer(self, article_list, method='nltk'):
+    def article_tokenizer(self, article_list, method='nltk',remove_stop_words=False):
         tokens = []
-        if method == 'nltk':
+        if remove_stop_words:
             for curr_text in article_list:
-                article_tokens = nltk.word_tokenize(curr_text)
-                tokens.append(article_tokens)
+                curr_tokens = nltk.word_tokenize( curr_text )
+                stop_words = stopwords.words('english')
+                tokens.append( [x for x in curr_tokens if x.lower() not in stop_words ])
+        else:
+            tokens.append( [ nltk.word_tokenize(curr_text) for curr_text in article_list  ])
         return tokens
 
 
-articles = ArticleLoader().articles
+article_list = ["A Human machine interface for lab abc computer applications","A survey of user opinion of computer system response time", "The EPS user interface management system","System and human system engineering testing of EPS","Relation of user perceived response time to error measurement"]
+y = Preprocess().article_tokenizer(article_list)
+print(y)
+y = Preprocess().article_tokenizer(article_list,remove_stop_words=True)
+print(y)
